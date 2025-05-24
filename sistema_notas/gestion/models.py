937b7ad1e3c8b7201fa465b1851c694890c2b455
unknown <chavezcunopaul@gmail.com>
2025-05-24 13:52:19 -0500

    Definimos el como sera nuestra base de datos con el archivo models.py, en este caso tendremos 3 clases

--- sistema_notas/gestion/models.py
+++ sistema_notas/gestion/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre}"
+
+class Nota(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso}: {self.nota}"
