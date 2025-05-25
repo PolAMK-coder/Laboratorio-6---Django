5b6dc258eb68a05354dc3722c92f71f55dc91622
SaulOchoa1406 <sochoar@unsa.edu.pe>
2025-05-24 20:57:36 -0500

    FUNCIÓN 1: agregar_alumno
    se define una función en Python llamada agregar_alumno.
    se comprueba si el método de la petición fue POST
    se crea un objeto llamado form usando los datos que el usuario envió.
    se verifica que el formulario sea válido.
    luego, redirige al usuario a la misma página (agregar_alumno), como si actualizara la página.
    si el método no es POST, se crea un formulario vacío para que el usuario lo llene.
    y al final se muestra al usuario la página HTML llamada agregar_alumno.html.

--- sistema_notas/gestion/views.py
+++ sistema_notas/gestion/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from .forms import AlumnoForm, CursoForm, NotaForm
 
+def agregar_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_alumno')
+    else:
+        form = AlumnoForm()
+    return render(request, 'agregar_alumno.html', {'form': form})
